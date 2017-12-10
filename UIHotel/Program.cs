3a09f6c8af6899e21d644824a7fc2cf2f35585ce
nekocoder <yogyphang@gmail.com>
2017-12-10 18:41:12 +0700

    Finish check in layout

--- UIHotel/Program.cs
+++ UIHotel/Program.cs
@@ -18,7 +18,7 @@ namespace UIHotel
             using (var startup = new App())
             {
                 startup.IsShowDevTool = true;
-                startup.Run("http://localhost.com/index.cshtml");     //Open after finish configure
+                startup.Run("http://localhost.com/checkin/checkin.cshtml");     //Open after finish configure
             }
         }
     }
