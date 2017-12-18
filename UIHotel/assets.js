9e86cbb4c71cab33fe05990726b9872256fb64c0
nothing628 <yogyphang@gmail.com>
2017-12-18 13:59:59 +0700

    migrate js

--- UIHotel/assets.js
+++ UIHotel/assets.js
@@ -1,6 +1,9 @@
 ﻿const CSS = [];
 const JS = [
-    'jquery/dist/jquery.min.js'
+    'jquery/dist/jquery.min.js',
+    'jquery.flot/jquery.flot.js',
+    'jquery.flot/jquery.flot.resize.js',
+    'moment/min/moment.min.js',
 ];
 
 module.exports = [...JS, ...CSS];
