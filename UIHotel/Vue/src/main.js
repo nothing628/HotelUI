bd269bf858a942962d0fb103f4317121b76ee410
nothing628 <yogyphang@gmail.com>
2018-01-01 09:25:06 +0700

    Add page

--- UIHotel/Vue/src/main.js
+++ UIHotel/Vue/src/main.js
@@ -4,12 +4,18 @@ import '../../node_modules/vuetify/dist/vuetify.min.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import router from './router'
+import Booking from './pages/booking/Booking'
+import Checkin from './pages/checkin/Checkin'
+import Checkout from './pages/checkin/Checkout'
 import RoomCategory from './pages/room/Category'
 import RoomList from './pages/room/List'
 import RoomMaintain from './pages/settings/Room'
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false
+Vue.component('booking', Booking)
+Vue.component('checkin', Checkin)
+Vue.component('checkout', Checkout)
 Vue.component('room-category', RoomCategory)
 Vue.component('room-list', RoomList)
 Vue.component("setting-room", RoomMaintain)
