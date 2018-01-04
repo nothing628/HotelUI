477141774da8bfc70358377863e837eded2ee35c
nothing628 <yogyphang@gmail.com>
2018-01-04 15:48:27 +0700

    Translate to vue page

--- UIHotel/Vue/src/pages/room/Detail.vue
+++ UIHotel/Vue/src/pages/room/Detail.vue
@@ -4,59 +4,69 @@
             <v-card-title>
                 <h2 class="card-title mb-0">Room Detail : Big No. 201</h2>
             </v-card-title>
-            <div class="card-block">
-                <div class="row">
-                    <div class="col-2">
+            <v-card-title class="card-block">
+                <v-layout row>
+                    <v-flex md3>
                         <img class="img-fluid" src="http://localhost.com/images/room.jpg" />
-                    </div>
-                    <div class="col-10">
-                        <form>
-                            <div class="form-group row">
-                                <label class="col-2 col-form-label">Room Type</label>
-                                <div class="col-4">
-                                    <select class="form-control">
-                                        <option>Room Type</option>
-                                        <option selected>Big</option>
-                                        <option>Medium</option>
-                                        <option>Small</option>
-                                    </select>
-                                </div>
-                            </div>
+                    </v-flex>
+                    <v-flex md9>
+                        <v-form>
+                            <v-layout row>
+                                <v-flex md2>
+                                    <v-subheader class="text--lighten-1">Room Type</v-subheader>
+                                </v-flex>
+                                <v-flex md4>
+                                    <v-text-field readonly value="BIG"></v-text-field>
+                                </v-flex>
+                            </v-layout>
 
-                            <div class="form-group row">
-                                <label class="col-2 col-form-label">Room Number</label>
-                                <div class="col-4">
-                                    <input class="form-control" placeholder="Room Number" value="201" type="text">
-                                </div>
-                            </div>
+                            <v-layout row>
+                                <v-flex md2>
+                                    <v-subheader class="text--lighten-1">Room Number</v-subheader>
+                                </v-flex>
+                                <v-flex md4>
+                                    <v-text-field readonly value="201"></v-text-field>
+                                </v-flex>
+                            </v-layout>
 
-                            <div class="form-group row">
-                                <label class="col-2 col-form-label">Room Status</label>
-                                <div class="col-4">
-                                    <input class="form-control" placeholder="" value="Vacant" type="text">
-                                </div>
-                            </div>
+                            <v-layout row>
+                                <v-flex md2>
+                                    <v-subheader class="text--lighten-1">Room Status</v-subheader>
+                                </v-flex>
+                                <v-flex md4>
+                                    <v-text-field readonly value="Vacant"></v-text-field>
+                                </v-flex>
+                            </v-layout>
 
-                            <div class="form-group row">
-                                <label class="col-2 col-form-label">Floor Number</label>
-                                <div class="col-4">
-                                    <input class="form-control" placeholder="Floor" value="1" type="number">
-                                </div>
-                            </div>
+                            <v-layout row>
+                                <v-flex md2>
+                                    <v-subheader class="text--lighten-1">Floor Number</v-subheader>
+                                </v-flex>
+                                <v-flex md4>
+                                    <v-text-field readonly value="1"></v-text-field>
+                                </v-flex>
+                            </v-layout>
 
-                            <div class="form-group row">
-                                <div class="col-10 offset-2">
-                                    <div class="btn-group-lg">
-                                        <a class="btn success" href="http://localhost.com/home/get/checkin"><i class="zmdi zmdi-download"></i> Checkin</a>
-                                        <a class="btn info" href="http://localhost.com/home/get/booking"><i class="zmdi zmdi-bookmark"></i> Booking</a>
-                                        <a class="btn error" href="http://localhost.com/home/get/checkout"><i class="zmdi zmdi-upload"></i> Checkout</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                            <v-layout row>
+                                <v-flex md10 offset-md2>
+                                    <v-btn color="green lighten-1" class="white--text" href="http://localhost.com/home/get/checkin">
+                                        Checkin
+                                        <v-icon right dark>file_download</v-icon>
+                                    </v-btn>
+                                    <v-btn color="light-blue darken-1" class="white--text" href="http://localhost.com/home/get/booking">
+                                        Booking
+                                        <v-icon right dark>bookmark</v-icon>
+                                    </v-btn>
+                                    <v-btn color="deep-orange darken-1" class="white--text" href="http://localhost.com/home/get/checkout">
+                                        Checkout
+                                        <v-icon right dark>file_upload</v-icon>
+                                    </v-btn>
+                                </v-flex>
+                            </v-layout>
+                        </v-form>
+                    </v-flex>
+                </v-layout>
+            </v-card-title>
         </v-card>
     </v-app>
 </template>
@@ -65,7 +75,6 @@
     export default {
         data() {
             return {
-                
             }
         }
     }
