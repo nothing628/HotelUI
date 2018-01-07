f25ba255dc10c14dfb048f101077b81a1dc5cb07
nothing628 <yogyphang@gmail.com>
2018-01-07 16:34:18 +0700

    bind checkin to model

--- UIHotel/Vue/src/pages/checkin/Checkin.vue
+++ UIHotel/Vue/src/pages/checkin/Checkin.vue
@@ -13,7 +13,7 @@
                             <v-subheader class="text--lighten-1">Booking Number</v-subheader>
                         </v-flex>
                         <v-flex md4>
-                            <v-text-field type="text"></v-text-field>
+                            <v-text-field type="text" readonly disabled v-model="registration.book_no"></v-text-field>
                         </v-flex>
                         <v-flex md2>
                             <v-btn fab dark small color="primary">
@@ -27,7 +27,7 @@
                             <v-subheader class="text--lighten-1">Deposit</v-subheader>
                         </v-flex>
                         <v-flex md3>
-                            <v-text-field type="number" prefix="Rp"></v-text-field>
+                            <v-text-field type="number" prefix="Rp" v-model="registration.deposit"></v-text-field>
                         </v-flex>
                     </v-layout>
 
@@ -42,10 +42,10 @@
                                       full-width
                                       width="290px">
                                 <v-text-field slot="activator"
-                                              v-model="date1"
+                                              v-model="registration.arr_date"
                                               prepend-icon="event"
                                               readonly></v-text-field>
-                                <v-date-picker v-model="date1" scrollable actions>
+                                <v-date-picker v-model="registration.arr_date" scrollable actions>
                                     <template slot-scope="{ save, cancel }">
                                         <v-card-actions>
                                             <v-spacer></v-spacer>
@@ -66,10 +66,10 @@
                                       full-width
                                       width="290px">
                                 <v-text-field slot="activator"
-                                              v-model="date2"
+                                              v-model="registration.dep_date"
                                               prepend-icon="event"
                                               readonly></v-text-field>
-                                <v-date-picker v-model="date2" scrollable actions>
+                                <v-date-picker v-model="registration.dep_date" scrollable actions>
                                     <template slot-scope="{ save, cancel }">
                                         <v-card-actions>
                                             <v-spacer></v-spacer>
@@ -87,13 +87,13 @@
                             <v-subheader class="text--lighten-1">Jumlah Adult</v-subheader>
                         </v-flex>
                         <v-flex md4>
-                            <v-text-field type="number" value="1"></v-text-field>
+                            <v-text-field type="number" v-model="registration.adl_count"></v-text-field>
                         </v-flex>
                         <v-flex md2>
                             <v-subheader class="text--lighten-1">Jumlah Child</v-subheader>
                         </v-flex>
                         <v-flex md4>
-                            <v-text-field type="number" value="0"></v-text-field>
+                            <v-text-field type="number" v-model="registration.chl_count"></v-text-field>
                         </v-flex>
                     </v-layout>
                 </v-form>
@@ -110,7 +110,7 @@
                             <v-subheader class="text--lighten-1">Select Room</v-subheader>
                         </v-flex>
                         <v-flex md6>
-                            <v-text-field type="text" readonly></v-text-field>
+                            <v-text-field type="text" readonly disabled v-model="room.room_number"></v-text-field>
                         </v-flex>
                         <v-flex md2>
                             <v-btn fab dark small color="primary">
@@ -124,7 +124,7 @@
                             <v-subheader class="text--lighten-1">Note</v-subheader>
                         </v-flex>
                         <v-flex md6>
-                            <v-text-field type="text" multi-line :rows="3"></v-text-field>
+                            <v-text-field type="text" multi-line :rows="3" v-model="room.note"></v-text-field>
                         </v-flex>
                     </v-layout>
                 </v-form>
@@ -141,7 +141,7 @@
                             <v-subheader class="text--lighten-1">ID Number*</v-subheader>
                         </v-flex>
                         <v-flex md6>
-                            <v-text-field type="text"></v-text-field>
+                            <v-text-field type="text" v-model="guest.id_number"></v-text-field>
                         </v-flex>
                         <v-flex md2>
                             <v-btn fab dark small color="primary">
@@ -149,22 +149,20 @@
                             </v-btn>
                         </v-flex>
                     </v-layout>
-
                     <v-layout row>
                         <v-flex md2>
                             <v-subheader class="text--lighten-1">Name*</v-subheader>
                         </v-flex>
                         <v-flex md8>
-                            <v-text-field type="text"></v-text-field>
+                            <v-text-field type="text" v-model="guest.name"></v-text-field>
                         </v-flex>
                     </v-layout>
-
                     <v-layout row>
                         <v-flex md2>
                             <v-subheader class="text--lighten-1">Guest Type</v-subheader>
                         </v-flex>
                         <v-flex md6>
-                            <v-radio-group row>
+                            <v-radio-group row v-model="guest.type">
                                 <v-radio label="Regular" value="Regular"></v-radio>
                                 <v-radio label="VIP" value="VIP"></v-radio>
                             </v-radio-group>
@@ -175,7 +173,7 @@
                             <v-subheader class="text--lighten-1">Birth Day</v-subheader>
                         </v-flex>
                         <v-flex md4>
-                            <v-text-field type="text" label="Birth Place"></v-text-field>
+                            <v-text-field type="text" label="Birth Place" v-model="guest.birth_place"></v-text-field>
                         </v-flex>
                         <v-flex md4>
                             <v-dialog persistent
@@ -184,10 +182,10 @@
                                       full-width
                                       width="290px">
                                 <v-text-field slot="activator"
-                                              v-model="date3"
+                                              v-model="guest.birth_day"
                                               prepend-icon="event"
                                               readonly></v-text-field>
-                                <v-date-picker v-model="date3" scrollable actions>
+                                <v-date-picker v-model="guest.birth_day" scrollable actions>
                                     <template slot-scope="{ save, cancel }">
                                         <v-card-actions>
                                             <v-spacer></v-spacer>
@@ -204,7 +202,7 @@
                             <v-subheader class="text--lighten-1">Address</v-subheader>
                         </v-flex>
                         <v-flex md6>
-                            <v-text-field type="text" multi-line :rows="2"></v-text-field>
+                            <v-text-field type="text" multi-line :rows="2" v-model="guest.address.note"></v-text-field>
                         </v-flex>
                     </v-layout>
                     <v-layout row>
@@ -213,6 +211,7 @@
                         </v-flex>
                         <v-flex md4>
                             <v-select v-bind:items="['test']"
+                                      v-model="guest.address.state"
                                       label="Select"
                                       single-line
                                       auto
@@ -223,7 +222,7 @@
                             <v-subheader class="text--lighten-1">Province</v-subheader>
                         </v-flex>
                         <v-flex md4>
-                            <v-text-field type="text"></v-text-field>
+                            <v-text-field type="text" v-model="guest.address.province"></v-text-field>
                         </v-flex>
                     </v-layout>
                     <v-layout row>
@@ -231,13 +230,13 @@
                             <v-subheader class="text--lighten-1">City</v-subheader>
                         </v-flex>
                         <v-flex md4>
-                            <v-text-field type="text"></v-text-field>
+                            <v-text-field type="text" v-model="guest.address.city"></v-text-field>
                         </v-flex>
                         <v-flex md2>
                             <v-subheader class="text--lighten-1">Pos Code</v-subheader>
                         </v-flex>
                         <v-flex md4>
-                            <v-text-field type="text"></v-text-field>
+                            <v-text-field type="text" v-model="guest.address.postcode"></v-text-field>
                         </v-flex>
                     </v-layout>
                     <v-layout row>
@@ -245,7 +244,7 @@
                             <v-subheader class="text--lighten-1">Email</v-subheader>
                         </v-flex>
                         <v-flex md6>
-                            <v-text-field type="text"></v-text-field>
+                            <v-text-field type="text" v-model="guest.email"></v-text-field>
                         </v-flex>
                     </v-layout>
                     <v-layout row>
@@ -253,13 +252,13 @@
                             <v-subheader class="text--lighten-1">Phone Number 1*</v-subheader>
                         </v-flex>
                         <v-flex md4>
-                            <v-text-field type="text" prepend-icon="phone"></v-text-field>
+                            <v-text-field type="text" prepend-icon="phone" v-model="guest.phone.phone1"></v-text-field>
                         </v-flex>
                         <v-flex md2>
                             <v-subheader class="text--lighten-1">Phone Number 2</v-subheader>
                         </v-flex>
                         <v-flex md4>
-                            <v-text-field type="text" prepend-icon="phone"></v-text-field>
+                            <v-text-field type="text" prepend-icon="phone" v-model="guest.phone.phone2"></v-text-field>
                         </v-flex>
                     </v-layout>
                     <v-layout row>
@@ -293,26 +292,60 @@
     </v-app>
 </template>
 <script>
+    import axios from 'axios'
+    import moment from 'moment'
+
     export default {
         data() {
             return {
-                date1: null,
-                date2: null,
-                date3: null,
                 modal1: false,
                 modal2: false,
                 modal3: false,
                 registration : {
                     valid: true,
                     book_no: "",
+                    deposit: 50000,
+                    arr_date: null,
+                    dep_date: null,
+                    adl_count: 1,
+                    chl_count: 0
                 },
                 room: {
                     valid: true,
+                    room_number: null,
+                    note: null
                 },
                 guest: {
                     valid: true,
+                    id_number: null,
+                    name: null,
+                    type: 'Regular',
+                    birth_place: null,
+                    birth_day: null,
+                    address: {
+                        state: null,
+                        province: null,
+                        city: null,
+                        postcode: null,
+                        note: null
+                    },
+                    email: null,
+                    phone: {
+                        phone1: null,
+                        phone2: null
+                    },
+                    photo_doc: null,
+                    photo_guest: null
                 }
             }
+        },
+        watch: {
+        },
+        mounted() {
+            var momen = moment()
+
+            this.registration.arr_date = momen.format('YYYY-MM-DD')
+            this.registration.dep_date = momen.add(1, 'd').format('YYYY-MM-DD')
         }
     }
 </script>
