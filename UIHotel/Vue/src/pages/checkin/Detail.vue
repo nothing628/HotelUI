8eec2ec51b07740c9048e72d9252d9f813b1fa28
nothing628 <yogyphang@gmail.com>
2018-01-28 21:30:02 +0700

    update vuetify

--- UIHotel/Vue/src/pages/checkin/Detail.vue
+++ UIHotel/Vue/src/pages/checkin/Detail.vue
@@ -1,91 +1,103 @@
 <template>
     <div>
-        <v-card>
-            <v-card-title>
-                <h2 class="card-title mb-0">Checkin Detail</h2>
-            </v-card-title>
-
-            <v-container fluid grid-list-md>
-                <v-layout row>
-                    <v-flex lg3 md3 sm12 xs12>
-                        <img style="max-height: 190px;" class="img-fluid img-thumbnail rounded" :src="PhotoUrl" @error="imageError" />
-                    </v-flex>
-                    <v-flex lg9 md9 sm12 xs12>
+        <v-toolbar color="cyan" dark tabs>
+            <v-tabs color="cyan" slot="extension" v-model="tab" grow>
+                <v-tabs-slider color="yellow"></v-tabs-slider>
+                <v-tab>Detail</v-tab>
+                <v-tab>Invoice</v-tab>
+            </v-tabs>
+        </v-toolbar>
+        
+        <v-tabs-items v-model="tab">
+            <v-tab-item>
+                <v-card>
+                    <v-card-title>
+                        <h2 class="card-title mb-0">Checkin Detail</h2>
+                    </v-card-title>
+                    <v-container fluid grid-list-md>
                         <v-layout row>
-                            <v-flex xs3>
-                                <v-subheader>Checkin Number</v-subheader>
+                            <v-flex lg3 md3 sm12 xs12>
+                                <img style="max-height: 190px;" class="img-fluid img-thumbnail rounded" :src="PhotoUrl" @error="imageError" />
                             </v-flex>
-                            <v-flex xs4>
-                                <v-subheader>{{ checkin.Id }}</v-subheader>
+                            <v-flex lg9 md9 sm12 xs12>
+                                <v-layout row>
+                                    <v-flex xs3>
+                                        <v-subheader>Checkin Number</v-subheader>
+                                    </v-flex>
+                                    <v-flex xs4>
+                                        <v-subheader>{{ checkin.Id }}</v-subheader>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs3>
+                                        <v-subheader>Invoice Number</v-subheader>
+                                    </v-flex>
+                                    <v-flex xs4>
+                                        <v-subheader>{{ checkin.InvoiceId }}</v-subheader>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs3>
+                                        <v-subheader>Guest Name</v-subheader>
+                                    </v-flex>
+                                    <v-flex xs4>
+                                        <v-subheader>
+                                            <a :href="checkin.DetailGuest">{{ checkin.Fullname }}</a>
+                                        </v-subheader>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs3>
+                                        <v-subheader>Room Number</v-subheader>
+                                    </v-flex>
+                                    <v-flex xs4>
+                                        <v-subheader>
+                                            <a :href="checkin.DetailRoom">{{ checkin.RoomName }}</a>
+                                        </v-subheader>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs3>
+                                        <v-subheader>Checkin At</v-subheader>
+                                    </v-flex>
+                                    <v-flex xs4>
+                                        <v-subheader>{{ checkin.CheckinAt }}</v-subheader>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs3>
+                                        <v-subheader>Departure At</v-subheader>
+                                    </v-flex>
+                                    <v-flex xs4>
+                                        <v-subheader>{{ checkin.DepartureAt }}</v-subheader>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs3>
+                                        <v-subheader>Note</v-subheader>
+                                    </v-flex>
+                                    <v-flex xs9>
+                                        <v-subheader>{{ checkin.Note }}</v-subheader>
+                                    </v-flex>
+                                </v-layout>
                             </v-flex>
                         </v-layout>
+                    </v-container>
+                </v-card>
+            </v-tab-item>
+            <v-tab-item>
+                <v-card>
+                    <v-card-title>
+                        <h2 class="card-title mb-0">Invoice</h2>
+                    </v-card-title>
+                    <v-container fluid grid-list-md>
                         <v-layout row>
-                            <v-flex xs3>
-                                <v-subheader>Invoice Number</v-subheader>
-                            </v-flex>
-                            <v-flex xs4>
-                                <v-subheader>{{ checkin.InvoiceId }}</v-subheader>
-                            </v-flex>
+                            <v-flex lg12 md12 sm12 xs12></v-flex>
                         </v-layout>
-                        <v-layout row>
-                            <v-flex xs3>
-                                <v-subheader>Guest Name</v-subheader>
-                            </v-flex>
-                            <v-flex xs4>
-                                <v-subheader>
-                                    <a :href="checkin.DetailGuest">{{ checkin.Fullname }}</a>
-                                </v-subheader>
-                            </v-flex>
-                        </v-layout>
-                        <v-layout row>
-                            <v-flex xs3>
-                                <v-subheader>Room Number</v-subheader>
-                            </v-flex>
-                            <v-flex xs4>
-                                <v-subheader>
-                                    <a :href="checkin.DetailRoom">{{ checkin.RoomName }}</a>
-                                </v-subheader>
-                            </v-flex>
-                        </v-layout>
-                        <v-layout row>
-                            <v-flex xs3>
-                                <v-subheader>Checkin At</v-subheader>
-                            </v-flex>
-                            <v-flex xs4>
-                                <v-subheader>{{ checkin.CheckinAt }}</v-subheader>
-                            </v-flex>
-                        </v-layout>
-                        <v-layout row>
-                            <v-flex xs3>
-                                <v-subheader>Departure At</v-subheader>
-                            </v-flex>
-                            <v-flex xs4>
-                                <v-subheader>{{ checkin.DepartureAt }}</v-subheader>
-                            </v-flex>
-                        </v-layout>
-                        <v-layout row>
-                            <v-flex xs3>
-                                <v-subheader>Note</v-subheader>
-                            </v-flex>
-                            <v-flex xs9>
-                                <v-subheader>{{ checkin.Note }}</v-subheader>
-                            </v-flex>
-                        </v-layout>
-                    </v-flex>
-                </v-layout>
-            </v-container>
-        </v-card>
-        <v-card class="mt-2">
-            <v-card-title>
-                <h2 class="card-title mb-0">Invoice</h2>
-            </v-card-title>
-
-            <v-container fluid grid-list-md>
-                <v-layout row>
-                    <v-flex lg12 md12 sm12 xs12></v-flex>
-                </v-layout>
-            </v-container>
-        </v-card>
+                    </v-container>
+                </v-card>
+            </v-tab-item>
+        </v-tabs-items>
     </div>
 </template>
 <script>
@@ -93,6 +105,7 @@
     export default {
         data() {
             return {
+                tab: null,
                 checkin: {
                     Id: null,
                     Fullname: null,
