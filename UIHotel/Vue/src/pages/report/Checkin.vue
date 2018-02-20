9aec845ffc158b17c45ff45ceebf2b17c953a22c
nothing628 <yogyphang@gmail.com>
2018-02-20 22:02:02 +0700

    change template

--- UIHotel/Vue/src/pages/report/Checkin.vue
+++ UIHotel/Vue/src/pages/report/Checkin.vue
@@ -1,5 +1,17 @@
 <template>
-    <div></div>
+    <v-card class="mb-4">
+        <v-card-title primary-title>
+            <h2 class="card-title mb-0">REPORT CHECKIN & BOOKING</h2>
+        </v-card-title>
+
+        <v-container fluid grid-list-md>
+            <v-layout row>
+                <v-flex md12>
+
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-card>
 </template>
 <script>
     import axios from 'axios'
