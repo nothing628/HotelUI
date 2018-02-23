3dd893eb94f1b0a87fe6730cb5487adf7e2bef54
nothing628 <yogyphang@gmail.com>
2018-02-23 21:29:38 +0700

    Finish checkin report

--- UIHotel/App/Controller/ReportController.cs
+++ UIHotel/App/Controller/ReportController.cs
@@ -1,4 +1,5 @@
 ﻿using CefSharp;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UIHotel.App.Attributes;
 using UIHotel.Data;
+using UIHotel.Data.Table;
 
 namespace UIHotel.App.Controller
 {
@@ -65,7 +67,7 @@ namespace UIHotel.App.Controller
         {
             var token = jToken;
             var range_type = token.Value<string>("range_type");
-            var bdate = token.Value<DateTime?>("bdate");
+            var bdate = token.Value<DateTime>("bdate");
             var edate = token.Value<DateTime?>("edate");
 
             using (var model = new DataContext())
@@ -76,21 +78,124 @@ namespace UIHotel.App.Controller
                     {
                         case "d":
                             // Days
+                            edate = bdate.AddDays(1);
                             break;
                         case "m":
                             // Monthly
+                            bdate = new DateTime(bdate.Year, bdate.Month, 1);
+                            edate = bdate.AddMonths(1);
                             break;
                         case "y":
                             // Yearly
+                            bdate = new DateTime(bdate.Year, 1, 1);
+                            edate = bdate.AddYears(1);
                             break;
                     }
 
-                    return Json(new { success = true });
+                    var iBooking = (from a in model.Bookings
+                                    where a.CreateAt >= bdate
+                                    where a.CreateAt < edate
+                                    select a);
+                    var iCheckin = (from a in model.CheckIn
+                                    where a.CheckinAt >= bdate
+                                    where a.CheckinAt < edate
+                                    select a);
+                    var iCheckout = (from a in model.CheckIn
+                                     where a.CheckoutAt >= bdate
+                                     where a.CheckoutAt < edate
+                                     where a.CheckoutAt.HasValue
+                                     select a);
+
+                    var listBooking = iBooking.ToList();
+                    var listCheckin = iCheckin.ToList();
+                    var listCheckout = iCheckout.ToList();
+
+                    var grpCheckin = listCheckin
+                        .GroupBy(x => x.CheckinAt.Date)
+                        .Select(x => new CheckinResult()
+                        {
+                            Date = x.Key,
+                            ListCheckin = x.ToList()
+                        }).ToList();
+                    var grpCheckout = listCheckout
+                        .GroupBy(x => x.CheckoutAt.Value.Date)
+                        .Select(x => new CheckinResult()
+                        {
+                            Date = x.Key,
+                            ListCheckin = x.ToList()
+                        }).ToList();
+                    var grpBooking = listBooking
+                        .GroupBy(x => x.CreateAt.Date)
+                        .Select(x => new BookingResult()
+                        {
+                            Date = x.Key,
+                            ListBooking = x.ToList()
+                        }).ToList();
+
+                    var i = bdate;
+                    var data = new List<ReportResult>();
+
+                    do
+                    {
+                        var result = new ReportResult()
+                        {
+                            Date = i,
+                            BookingCount = (from a in grpBooking where a.Date == i select a.Count).FirstOrDefault(),
+                            CheckinCount = (from a in grpCheckin where a.Date == i select a.Count).FirstOrDefault(),
+                            CheckoutCount = (from a in grpCheckout where a.Date == i select a.Count).FirstOrDefault(),
+                        };
+
+                        data.Add(result);
+
+                        i = i.AddDays(1);
+                    } while (i <= edate);
+
+                    return Json(new { success = true, data });
                 } catch
                 {
                     return Json(new { success = false });
                 }
             }
         }
+    }
+
+    public class BookingResult
+    {
+        public DateTime Date { get; set; }
+
+        [JsonIgnore]
+        public List<Booking> ListBooking { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return ListBooking.Sum(x => x.CountAdult) + ListBooking.Sum(x => x.CountChild);
+            }
+        }
+    }
+
+    public class CheckinResult
+    {
+        public DateTime Date { get; set; }
+
+        [JsonIgnore]
+        public List<Checkin> ListCheckin { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return ListCheckin.Sum(x => x.CountAdult) + ListCheckin.Sum(x => x.CountChild);
+            }
+        }
+    }
+
+    public class ReportResult
+    {
+        public DateTime Date { get; set; }
+        public int CheckinCount { get; set; }
+        public int CheckoutCount { get; set; }
+        public int BookingCount { get; set; }
     }
 }
--- UIHotel/vue/src/pages/report/Checkin.vue
+++ UIHotel/vue/src/pages/report/Checkin.vue
@@ -7,11 +7,26 @@
         <v-container fluid grid-list-md>
             <v-layout row>
                 <v-flex md12>
+                    <label>Range :</label>
+                    <v-btn-toggle v-model="range_type">
+                        <v-btn flat value="c">
+                            Custom
+                        </v-btn>
+                        <v-btn flat value="d">
+                            Daily
+                        </v-btn>
+                        <v-btn flat value="m">
+                            Monthly
+                        </v-btn>
+                        <v-btn flat value="y">
+                            Yearly
+                        </v-btn>
+                    </v-btn-toggle>
                     <v-menu ref="menu1"
-                            lazy
                             :close-on-content-click="false"
                             v-model="modal1"
                             transition="scale-transition"
+                            offset-y
                             :nudge-right="40"
                             min-width="310px"
                             :return-value.sync="bdate">
@@ -23,9 +38,9 @@
                             <v-btn flat color="primary" @click="$refs.menu1.save(bdate)">OK</v-btn>
                         </v-date-picker>
                     </v-menu>
-                    s/d
-                    <v-menu ref="menu2"
-                            lazy
+                    <label v-show="range_type == 'c'">s/d</label>
+                    <v-menu v-show="range_type == 'c'"
+                            ref="menu2"
                             :close-on-content-click="false"
                             v-model="modal2"
                             transition="scale-transition"
@@ -41,25 +56,22 @@
                             <v-btn flat color="primary" @click="$refs.menu2.save(edate)">OK</v-btn>
                         </v-date-picker>
                     </v-menu>
-                    Range :
-                    <v-btn-toggle v-model="range_type">
-                        <v-btn flat value="d">
-                            Days
-                        </v-btn>
-                        <v-btn flat value="m">
-                            Monthly
-                        </v-btn>
-                        <v-btn flat value="y">
-                            Yearly
-                        </v-btn>
-                    </v-btn-toggle>
                     <v-btn color="primary" @click="getData">Search</v-btn>
                 </v-flex>
             </v-layout>
             <v-divider></v-divider>
             <v-layout row>
                 <v-flex md12>
-                    Checkin
+                    <v-data-table v-bind:headers="headers"
+                                  v-bind:items="items"
+                                  class="elevation-1">
+                        <template slot="items" slot-scope="props">
+                            <td>{{ props.item.Date | dateString }}</td>
+                            <td>{{ props.item.BookingCount }}</td>
+                            <td>{{ props.item.CheckinCount }}</td>
+                            <td>{{ props.item.CheckoutCount }}</td>
+                        </template>
+                    </v-data-table>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -76,29 +88,65 @@
                 modal2: false,
                 bdate: null,
                 edate: null,
-                range_type: 'd',
+                range_type: 'c',
+                items: [],
+                headers: [
+                    { text: 'Date', sortable: false, align: 'left', },
+                    { text: 'Booking', sortable: false, align: 'left' },
+                    { text: 'Checkin', sortable: false, align: 'left' },
+                    { text: 'Chekout', sortable: false, align: 'left' },
+                ]
             }
         },
         computed: {
             allowArr() {
                 return moment().format('YYYY-MM-DD')
+            },
+            calcEdate() {
+                let momen = moment(this.bdate)
+
+                switch (this.range_type) {
+                    case 'c':
+                        return this.edate
+                        break
+                    case 'd':
+                        return this.bdate
+                        break
+                    case 'm':
+                        return momen.format('YYYY-MM-DD')
+                        break
+                    case 'y':
+                        return momen.format('YYYY-MM-DD')
+                        break
+                }
+            }
+        },
+        filters: {
+            dateString(val) {
+                let momen = moment(val)
+
+                return momen.format('YYYY-MM-DD')
             }
         },
         methods: {
             getData() {
                 let data = {
                     bdate: this.bdate,
-                    edate: this.edate,
+                    edate: this.calcEdate,
                     range_type: this.range_type
                 }
 
-                axios.post('http://localhost.com/report/post/getReportCheckin', data).then(this.getReportCheckin)
+                axios.post('http://localhost.com/report/post/getReportCheckin', data).then(this.getDataData)
             },
             getDataData(response) {
                 let data = response.data
 
                 if (data.success) {
+                    let items = data.data
 
+                    this.items = []
+
+                    items.forEach(x => this.items.push(x))
                     console.log(data)
                 }
             }
