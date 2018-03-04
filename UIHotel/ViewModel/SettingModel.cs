66a12a01d9184ad8908c23f2ee6c5dd8c651d9c0
nothing628 <yogyphang@gmail.com>
2018-03-04 13:02:54 +0700

    add validation version

--- UIHotel/ViewModel/SettingModel.cs
+++ UIHotel/ViewModel/SettingModel.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,18 @@ namespace UIHotel.ViewModel
                 {
                     conn.Open();
 
+                    var dset = new DataTable();
+                    var command = new MySqlCommand("SELECT MAX(Version) FROM versioninfo", conn);
+                    var adapter = new MySqlDataAdapter(command);
+
+                    adapter.Fill(dset);
+
+                    var max = (long)dset.Rows[0][0];
+
+                    if (max != 6) //TODO: Need to find max of latest migration
+                    {
+                        connOK = false;
+                    }
                 } catch
                 {
                     connOK = false;
