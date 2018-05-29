4a2d72d9cbe3a0e887b55d0e08cb55043b81c579
nothing628 <yogyphang@gmail.com>
2018-05-29 22:18:44 +0700

    Add new netdimension

---
+++ UIHotel2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using Chromium.WebBrowser;
+using NetDimension.NanUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIHotel2
+{
+    public partial class Form1 : Formium
+    {
+        public Form1() : base("http://assets.app.local/index.html")
+        {
+            InitializeComponent();
+
+            GlobalObject.AddFunction("showDevTools").Execute += (func, args) => Chromium.ShowDevTools();
+
+            var obj = GlobalObject.AddObject("CS");
+            var prop = obj.AddDynamicProperty("new");
+
+            var fun = obj.AddFunction("test2");
+            fun.Execute += Fun_Execute;
+        }
+
+        private void Fun_Execute(object sender, Chromium.Remote.Event.CfrV8HandlerExecuteEventArgs e)
+        {
+            e.SetReturnValue("S");
+        }
+    }
+}
---
+++ UIHotel2/Program.cs
@@ -0,0 +1,29 @@
+﻿using NetDimension.NanUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIHotel2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            if (Bootstrap.Load())
+            {
+                Bootstrap.RegisterFolderResources("Assets");
+
+                Application.Run(new Form1());
+            }
+        }
+    }
+}
