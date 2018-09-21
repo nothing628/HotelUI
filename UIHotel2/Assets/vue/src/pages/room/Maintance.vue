4e6ee90c5e3bdf423c58e4cedfe66202e1e96cc6
nothing628 <yogyphang@gmail.com>
2018-09-21 23:41:27 +0700

    finish function room maintain

--- UIHotel2/Assets/vue/src/pages/room/Maintance.vue
+++ UIHotel2/Assets/vue/src/pages/room/Maintance.vue
@@ -7,40 +7,102 @@
       <div class="panel-body">
         <div class="dataTables_wrapper form-inline dt-bootstrap no-footer">
           <div class="dt-buttons btn-group">
-            <a class="btn btn-success buttons-copy buttons-html5 btn-sm" tabindex="0">
+            <a class="btn btn-success buttons-copy buttons-html5 btn-sm" @click="addData">
               <i class="fa fa-plus"></i>
               <span> Add Room</span>
             </a>
           </div>
           <div class="dataTables_filter">
-            <label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="data-table"></label>
+            <label>Search:<input type="search" class="form-control input-sm" placeholder="" ></label>
           </div>
-          <table class="table table-striped table-bordered dataTable no-footer dtr-inline" role="grid" aria-describedby="data-table_info">
+          <table class="table table-striped table-bordered dataTable no-footer dtr-inline" role="grid">
             <thead>
                 <tr role="row">
-                  <th tabindex="0" rowspan="1" colspan="1">Room Number</th>
-                  <th tabindex="0" rowspan="1" colspan="1">Room Category</th>
-                  <th tabindex="0" rowspan="1" colspan="1">Room Floor</th>
-                  <th tabindex="0" rowspan="1" colspan="1"></th>
+                  <th rowspan="1" colspan="1">Room Number</th>
+                  <th rowspan="1" colspan="1">Room Category</th>
+                  <th rowspan="1" colspan="1">Room Floor</th>
+                  <th rowspan="1" colspan="1">Status</th>
+                  <th rowspan="1" colspan="1"></th>
                 </tr>
             </thead>
             <tbody>
-              <tr>
-                  <td>202</td>
-                  <td>MEDIUM</td>
-                  <td>1</td>
+              <tr v-for="item in items" :key="item.Id">
+                  <td>{{ item.RoomNumber }}</td>
+                  <td>{{ item.CategoryName }}</td>
+                  <td>{{ item.RoomFloor }}</td>
+                  <td>{{ item.StateName }}</td>
                   <td>
                     <div class="btn-group pull-right">
-                      <button class="btn btn-sm btn-warning"><i class="fa fa-pencil"></i> Edit</button>
-                      <button class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> Delete</button>
+                      <button class="btn btn-sm btn-warning" @click="editData(item)"><i class="fa fa-pencil"></i> Edit</button>
+                      <button class="btn btn-sm btn-danger" @click="deleteData(item)"><i class="fa fa-trash"></i> Delete</button>
                     </div>
                   </td>
               </tr>
             </tbody>
           </table>
-          <counter></counter>
-          <pagination :total-page="totalPage" v-model="currentPage"></pagination>
+          <counter :from.sync="from" :to.sync="to" :total.sync="max_item"></counter>
+          <pagination :total-page.sync="totalPage" v-model="currentPage"></pagination>
         </div>
+        
+        <uiv-modal v-model="open_add" title="Add Room">
+          <div class="form-horizontal">
+            <div class="form-group">
+              <label class="col-md-4 control-label">Room Number</label>
+              <div class="col-md-4">
+                <input class="form-control" type="text" v-model="modal_data.RoomNumber" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-md-4 control-label">Room Floor</label>
+              <div class="col-md-4">
+                <input class="form-control" type="text" v-model="modal_data.RoomFloor" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-md-4 control-label">Category</label>
+              <div class="col-md-6">
+                <select class="form-control" v-model="modal_data.RoomCategoryId">
+                  <option v-for="item in categories" :key="item.Id" :value="item.Id">{{ item.CategoryName }}</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          
+          <template slot="footer">
+            <button class="btn btn-success" @click="storeData">Save</button>
+            <button class="btn btn-danger" @click="closeAll">Cancel</button>
+          </template>
+        </uiv-modal>
+
+        <uiv-modal v-model="open_edit" title="Edit Room">
+          <div class="form-horizontal">
+            <div class="form-group">
+              <label class="col-md-4 control-label">Room Number</label>
+              <div class="col-md-4">
+                <input class="form-control" type="text" v-model="modal_data.RoomNumber" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-md-4 control-label">Room Floor</label>
+              <div class="col-md-4">
+                <input class="form-control" type="text" v-model="modal_data.RoomFloor" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-md-4 control-label">Category</label>
+              <div class="col-md-6">
+                <select class="form-control" v-model="modal_data.RoomCategoryId">
+                  <option v-for="item in categories" :key="item.Id" :value="item.Id">{{ item.CategoryName }}</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          
+          <template slot="footer">
+            <button class="btn btn-success" @click="updateData">Save</button>
+            <button class="btn btn-danger" @click="closeAll">Cancel</button>
+          </template>
+        </uiv-modal>
       </div>
     </div>
   </div>
@@ -51,6 +113,14 @@ import { squel, ss, su, sd, si, execute, executeScalar } from "@/lib/Test";
 import Pagination from "@/components/Table/Pagination.vue";
 import Counter from "@/components/Table/Counter.vue";
 
+interface IModalData {
+  RoomNumber: string;
+  RoomFloor: string;
+  RoomStateId: number;
+  RoomCategoryId: number;
+  Id: number;
+}
+
 @Component({
   components: {
     Pagination,
@@ -58,11 +128,113 @@ import Counter from "@/components/Table/Counter.vue";
   }
 })
 export default class RoomMaintance extends Vue {
+  private open_add: boolean = false;
+  private open_edit: boolean = false;
   private max_item: number = 0;
-  private totalPage: number = 12;
+  private limit: number = 10;
   private currentPage: number = 1;
   private categories: Array<any> = new Array();
   private items: Array<any> = new Array();
+  private modal_data: IModalData = {
+    RoomStateId: 1,
+    RoomCategoryId: 0,
+    RoomNumber: "",
+    RoomFloor: "",
+    Id: 0
+  };
+
+  get totalPage(): number {
+    return Math.ceil(this.max_item / this.limit);
+  }
+
+  get from(): number {
+    return (this.currentPage - 1) * this.limit + 1;
+  }
+
+  get to(): number {
+    let rest = this.from + 9;
+
+    if (rest > this.max_item) return this.max_item;
+    return this.from + 9;
+  }
+
+  get offset(): number {
+    return (this.currentPage - 1) * this.limit;
+  }
+
+  closeAll() {
+    this.open_add = false;
+    this.open_edit = false;
+  }
+
+  deleteData(item: IModalData) {
+    let qry = sd()
+      .from("rooms")
+      .where("Id = ?", item.Id);
+    let result = executeScalar(qry);
+
+    this.getMaxItem();
+    this.getItems();
+  }
+
+  editData(item: IModalData) {
+    this.modal_data.Id = item.Id;
+    this.modal_data.RoomNumber = item.RoomNumber;
+    this.modal_data.RoomFloor = item.RoomFloor;
+    this.modal_data.RoomStateId = item.RoomStateId;
+    this.modal_data.RoomCategoryId = item.RoomCategoryId;
+    this.open_edit = true;
+  }
+
+  addData() {
+    this.modal_data.Id = 0;
+    this.modal_data.RoomNumber = "";
+    this.modal_data.RoomFloor = "";
+    this.modal_data.RoomStateId = 1;
+    this.modal_data.RoomCategoryId = 0;
+    this.open_add = true;
+  }
+
+  updateData() {
+    let qry = su()
+      .table("rooms")
+      .set("RoomNumber", this.modal_data.RoomNumber)
+      .set("RoomFloor", this.modal_data.RoomFloor)
+      .set("RoomCategoryId", this.modal_data.RoomCategoryId)
+      .where("Id = ?", this.modal_data.Id);
+    let result = executeScalar(qry);
+
+    this.getMaxItem();
+    this.getItems();
+    this.closeAll();
+  }
+
+  storeData() {
+    this.modal_data.RoomStateId = this.getStates();
+
+    let qry = si()
+      .into("rooms")
+      .set("RoomNumber", this.modal_data.RoomNumber)
+      .set("RoomFloor", this.modal_data.RoomFloor)
+      .set("RoomStateId", this.modal_data.RoomStateId)
+      .set("RoomCategoryId", this.modal_data.RoomCategoryId);
+    let result = executeScalar(qry);
+
+    this.getMaxItem();
+    this.getItems();
+    this.closeAll();
+  }
+
+  getStates(): number {
+    let qry = ss()
+      .from("roomstates")
+      .where("StateName = ?", "Vacant")
+      .field("Id");
+    let result = execute(qry);
+    let first = result[0];
+
+    return first.Id;
+  }
 
   getCategories() {
     let qry = ss().from("roomcategories");
@@ -71,14 +243,14 @@ export default class RoomMaintance extends Vue {
     result.forEach((item: any) => this.categories.push(item));
   }
 
-  getMaxItem(): number {
+  getMaxItem() {
     let qry = ss()
       .from("rooms")
-      .field("COUNT(*)");
+      .field("COUNT(*) as cnt");
     let result = execute(qry);
     let first = result[0];
 
-    return Number(Object.values(first)[0]);
+    this.max_item = Number(first.cnt);
   }
 
   getItems() {
@@ -90,24 +262,25 @@ export default class RoomMaintance extends Vue {
       .field("b.CategoryName")
       .field("c.StateName")
       .field("c.StateColor")
-      .limit(10)
-      .offset(0);
+      .limit(this.limit)
+      .offset(this.offset);
     let result = execute(qry);
 
-    console.log(result);
+    this.items = [];
+    result.forEach((item: any) => this.items.push(item));
   }
 
   mounted() {
     this.$store.commit("changeTitle", "Room Maintance");
     this.$store.commit("changeSubtitle", "");
-    this.max_item = this.getMaxItem();
     this.getCategories();
+    this.getMaxItem();
     this.getItems();
   }
 
   @Watch("currentPage")
   currentPageChanged(newval: number, oldval: number) {
-    console.log(newval);
+    this.getItems();
   }
 }
 </script>
