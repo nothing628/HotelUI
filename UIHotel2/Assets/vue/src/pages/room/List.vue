ba42663f0ec6e8d5d946bf58fbc1dd0e86bbb0c7
yogy phang <yogyphang@gmail.com>
2018-09-25 16:26:50 +0700

    list

--- UIHotel2/Assets/vue/src/pages/room/List.vue
+++ UIHotel2/Assets/vue/src/pages/room/List.vue
@@ -1,96 +1,99 @@
 <template>
-  <div class="panel-group" id="accordion">
+  <div class="panel-group">
     <div class="panel panel-inverse overflow-hidden">
       <div class="panel-heading">
         <h3 class="panel-title">
-          <a class="accordion-toggle accordion-toggle-styled" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+          <a class="accordion-toggle accordion-toggle-styled">
             <i class="fa fa-plus-circle pull-right"></i>CATEGORY : BIG
           </a>
         </h3>
       </div>
-      <div id="collapseOne" class="panel-collapse collapse">
-        <div class="panel-body">
-          <div class="row">
-            <div class="col-md-2">
-              <div class="widget widget-stats bg-green">
-                <div class="stats-info">
-                  <h4>READY</h4>
-                  <p>201</p>	
-                </div>
-                <div class="stats-link">
-                  <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-2">
-              <div class="widget widget-stats bg-orange">
-                <div class="stats-info">
-                  <h4>BOOKED</h4>
-                  <p>202</p>	
-                </div>
-                <div class="stats-link">
-                  <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+      <transition name="slide-fade">
+        <div v-show="is_show" class="panel-collapse collapse in">
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-md-2">
+                <div class="widget widget-stats bg-green">
+                  <div class="stats-info">
+                    <h4>READY</h4>
+                    <p>201</p>	
+                  </div>
+                  <div class="stats-link">
+                    <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="col-md-2">
-              <div class="widget widget-stats bg-red">
-                <div class="stats-info">
-                  <h4>OCCUPIED</h4>
-                  <p>203</p>	
-                </div>
-                <div class="stats-link">
-                  <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-2">
-              <div class="widget widget-stats bg-blue">
-                <div class="stats-info">
-                  <h4>LATE CHECKOUT</h4>
-                  <p>204</p>	
-                </div>
-                <div class="stats-link">
-                  <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+              <div class="col-md-2">
+                <div class="widget widget-stats bg-orange">
+                  <div class="stats-info">
+                    <h4>BOOKED</h4>
+                    <p>202</p>	
+                  </div>
+                  <div class="stats-link">
+                    <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="col-md-2">
-              <div class="widget widget-stats bg-purple">
-                <div class="stats-info">
-                  <h4>CLEANING</h4>
-                  <p>205</p>	
-                </div>
-                <div class="stats-link">
-                  <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+              <div class="col-md-2">
+                <div class="widget widget-stats bg-red">
+                  <div class="stats-info">
+                    <h4>OCCUPIED</h4>
+                    <p>203</p>	
+                  </div>
+                  <div class="stats-link">
+                    <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="col-md-2">
-              <div class="widget widget-stats bg-grey-darker">
-                <div class="stats-info">
-                  <h4>MAINTANCE</h4>
-                  <p>206</p>	
+              <div class="col-md-2">
+                <div class="widget widget-stats bg-blue">
+                  <div class="stats-info">
+                    <h4>LATE CHECKOUT</h4>
+                    <p>204</p>	
+                  </div>
+                  <div class="stats-link">
+                    <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+                  </div>
                 </div>
-                <div class="stats-link">
-                  <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+              </div>
+              <div class="col-md-2">
+                <div class="widget widget-stats bg-purple">
+                  <div class="stats-info">
+                    <h4>CLEANING</h4>
+                    <p>205</p>	
+                  </div>
+                  <div class="stats-link">
+                    <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-2">
+                <div class="widget widget-stats bg-grey-darker">
+                  <div class="stats-info">
+                    <h4>MAINTANCE</h4>
+                    <p>206</p>	
+                  </div>
+                  <div class="stats-link">
+                    <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </transition>
     </div>
     <div class="panel panel-inverse overflow-hidden">
       <div class="panel-heading">
         <h3 class="panel-title">
-          <a class="accordion-toggle accordion-toggle-styled" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+          <a class="accordion-toggle accordion-toggle-styled">
             <i class="fa fa-plus-circle pull-right"></i>CATEGORY : MEDIUM
           </a>
         </h3>
       </div>
-      <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-collapse collapse in">
         <div class="panel-body">
+          <button @click="is_show = !is_show">Toggle</button>
           <div class="row">
             <div class="col-md-2">
               <div class="widget widget-stats bg-green">
@@ -164,11 +167,24 @@
     </div>
   </div>
 </template>
+<style>
+.slide-fade-leave-active,
+.slide-fade-enter-active {
+  transition: all 0.6s ease;
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+  height: 0;
+  opacity: 0;
+}
+</style>
+
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
 
 @Component
 export default class RoomList extends Vue {
+  private is_show: boolean = false;
   mounted() {
     this.$store.commit("changeTitle", "Room List");
     this.$store.commit("changeSubtitle", "");
