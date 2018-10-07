d746a81c8ccd7d5dc3f4478ae1a84c3053c89a1d
nothing628 <yogyphang@gmail.com>
2018-10-07 21:56:26 +0700

    add notify component

---
+++ UIHotel2/Assets/vue/src/components/Layout/Notification.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="gritter-notice-wrapper">
+    <item :id="12" title="test" content="Test 12" v-for="notify in ListNotification" :key="notify.Id"></item>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Item from "@/components/Layout/NotificationItem.vue";
+
+interface INotify {
+  Title: string;
+  Content: string;
+  Id: number;
+  Type?: string;
+}
+
+@Component({
+  components: {
+    Item
+  }
+})
+export default class Notification extends Vue {
+  ListNotification: Array<INotify> = new Array<INotify>();
+
+  add(data: INotify) {
+    this.ListNotification.push(data);
+  }
+
+  mounted() {
+    //
+  }
+}
+</script>
---
+++ UIHotel2/Assets/vue/src/components/Layout/NotificationItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div :class="GritterClass">
+    <div class="gritter-top"></div>
+    <div class="gritter-item">
+      <a class="gritter-close" href="javascript:;">Close Notification</a>
+      <div>
+        <span class="gritter-title" v-html="Title"></span>
+        <p v-html="Content"></p>
+      </div>
+      <div style="clear:both"></div>
+    </div>
+    <div class="gritter-bottom"></div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class NotificationItem extends Vue {
+  @Prop({ required: true })
+  public Id?: number;
+
+  @Prop({ required: false, default: "" })
+  public Title?: string;
+
+  @Prop({ required: false, default: "" })
+  public Content?: string;
+
+  @Prop({ required: false, default: ""})
+  public Type?: string;
+
+  get GritterClass() {
+    let default_class = ["gritter-item-wrapper"];
+
+    switch(this.Type) {
+      case "success":
+        default_class.push("gritter-success");
+        break;
+      case "warning":
+        default_class.push("gritter-warning");
+        break;
+      case "error":
+        default_class.push("gritter-error");
+        break;
+      case "info":
+        default_class.push("gritter-info");
+        break;
+    }
+    return default_class;
+  }
+
+  mounted() {
+    //
+  }
+}
+</script>
